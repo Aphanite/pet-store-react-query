0ad776bc00128f370916fe1295d87db8494d7740
Aphanite <ena.rajovic@online.de>
2024-07-26 15:54:48 +0200

    Delete image fetching

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -1,19 +1,9 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Album } from "./Albums";
 
 export function AlbumCard(album: Album) {
   const { id, title } = album;
   const queryClient = useQueryClient();
-
-  const { isPending, isError, isSuccess, data } = useQuery({
-    queryKey: [`album-photo-${id}`],
-    queryFn: async () => {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/albums/${id}/photos`
-      );
-      return await response.json();
-    },
-  });
 
   const deleteMutation = useMutation({
     mutationFn: async (albumId: number) => {
@@ -54,22 +44,8 @@ export function AlbumCard(album: Album) {
     },
   });
 
-  console.log("data", deleteMutation?.data);
-  const firstImage = data?.[0];
-
   return (
     <div key={id} className="p-4 bg-slate-200 gap-2 justify-center">
-      {isPending && <p>Loading image...</p>}
-      {isError && <p>Couldn't load image</p>}
-      {isSuccess && (
-        <img
-          src={firstImage.url}
-          alt="album"
-          width="50"
-          height="50"
-          className="mx-auto"
-        />
-      )}
       <h3 className="uppercase font-bold">{title}</h3>
       <button
         aria-label="delete-album"
